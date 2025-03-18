f42376162d364325450d95063338ad97abba98ec
Sachin Chaunal <155889006+sachinchaunal@users.noreply.github.com>
2025-03-18 09:10:38 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://shopease-shopmanagementsystem-backend.onrender.com/api',
+   baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
   headers: {
     'Content-Type': 'application/json'
   },
